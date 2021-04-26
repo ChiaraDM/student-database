e820df44d182b551e2fdd2031bf7b4fb04e75f6f
ChiaraDM <demattec@roehampton.ac.uk>
2021-04-26 11:34:19 +0100

    Angular.js controlleres

--- static/index.js
+++ static/index.js
@@ -1,6 +1,26 @@
 "use strict";
 
-function printStudents() {
-    var main = document.getElementById("main");
-    main.innerHTML = "Hello, world!";
-};
+// Definition of student type
+class Student {
+    // ID of the student
+    id;
+    // Name of the student
+    name;
+
+    // Creates a new Student object
+    constructor(id,name) {
+        this.id = id;
+        this.name = name;
+    }
+} 
+
+// Gets the mainApp section of the HTML document
+var mainApp = angular.module("mainApp", []);
+
+// Creates the controller for the main application.
+mainApp.controller("studentController", function($scope) {
+    // Sets a new model value --student.
+    $scope.students = [
+        new Student("001", "Kevin Chalmers"),
+        new Student("002", "Chiara De Matteis")];
+});
