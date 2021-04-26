44173442e9102917d9d11563b867256e135d5e31
ChiaraDM <demattec@roehampton.ac.uk>
2021-04-26 15:02:42 +0100

    an event (from Kevin to Lisa)

--- static/index.js
+++ static/index.js
@@ -20,7 +20,13 @@ var mainApp = angular.module("mainApp", []);
 // Creates the controller for the main application.
 mainApp.controller("studentController", function($scope) {
     // Sets a new model value --student.
-    $scope.students = [
+    $scope.student = new Student("001", "Kevin Chalmers");
+    /*$scope.students = [
         new Student("001", "Kevin Chalmers"),
-        new Student("002", "Chiara De Matteis")];
+        new Student("002", "Chiara De Matteis")];*/
+    // Sets the update() function for the scope. Used by the button.
+    $scope.update = function() {
+        $scope.student.id = "002";
+        $scope.student.name = "Lisa Haskel";
+    }
 });
